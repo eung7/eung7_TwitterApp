a91f6c6f1b1e84d01c5575d78db390bc1792651a
eung7 <wannabeeung@gmail.com>
2022-03-21 21:53:28 +0900

    FeedTableViewCell의 UI들을 SnapKit을 통해 AutoLayout을 잡아주는 로직을 구현

--- eung7 Twitter App/FeedViewController/FeedTableViewCell.swift
+++ eung7 Twitter App/FeedViewController/FeedTableViewCell.swift
@@ -15,20 +15,63 @@ class FeedTableViewCell : UITableViewCell {
     let contentsLabel = UILabel()
     let profileImageView = UIImageView()
     
-    let heartIcon = UIImageView(image: UIImage(systemName: "heart"))
-    let messageIcon = UIImageView(image: UIImage(systemName: "message"))
-    let shareIcon = UIImageView(image: UIImage(systemName: "square.and.arrow.up"))
+    let heartButton = UIButton()
+    let messageButton = UIButton()
+    let shareButton = UIButton()
     
     func setupLayout() {
-        [ usernameLabel, accountLabel, contentsLabel, profileImageView, heartIcon, messageIcon, shareIcon ]
+        [ usernameLabel, accountLabel, contentsLabel, profileImageView, heartButton, messageButton, shareButton ]
             .forEach { contentView.addSubview($0) }
         
         profileImageView.backgroundColor = .gray
+        profileImageView.layer.cornerRadius = 25
         profileImageView.snp.makeConstraints {
             $0.top.equalToSuperview().inset(16)
             $0.leading.equalToSuperview().inset(16)
             $0.width.height.equalTo(50)
         }
+        
+        usernameLabel.font = .systemFont(ofSize: 14, weight: .bold)
+        usernameLabel.text = "KimEungCheol"
+        usernameLabel.snp.makeConstraints {
+            $0.top.equalTo(profileImageView.snp.top)
+            $0.leading.equalTo(profileImageView.snp.trailing).offset(16)
+        }
+        
+        accountLabel.font = .systemFont(ofSize: 12, weight: .light)
+        accountLabel.textColor = .secondaryLabel
+        accountLabel.text = "@eung7"
+        accountLabel.snp.makeConstraints {
+            $0.centerY.equalTo(usernameLabel)
+            $0.leading.equalTo(usernameLabel.snp.trailing).offset(4)
+        }
+        
+        contentsLabel.font = .systemFont(ofSize: 14)
+        contentsLabel.text = "Good night !"
+        contentsLabel.snp.makeConstraints {
+            $0.top.equalTo(usernameLabel.snp.bottom).offset(4)
+            $0.leading.equalTo(usernameLabel.snp.leading)
+        }
+
+        messageButton.setImage(UIImage(systemName: "message"), for: .normal)
+        messageButton.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.bottom.equalToSuperview().inset(16)
+            $0.width.height.equalTo(20)
+        }
+
+        heartButton.setImage(UIImage(systemName: "heart"), for: .normal)
+        heartButton.snp.makeConstraints {
+            $0.leading.equalTo(usernameLabel.snp.leading).offset(15)
+            $0.bottom.equalToSuperview().inset(16)
+            $0.width.height.equalTo(20)
+        }
+
+        shareButton.setImage(UIImage(systemName: "square.and.arrow.up"), for: .normal)
+        shareButton.snp.makeConstraints {
+            $0.leading.equalTo(messageButton.snp.trailing).offset(60)
+            $0.bottom.equalToSuperview().inset(16)
+            $0.width.height.equalTo(20)
+        }
     }
 }
-
