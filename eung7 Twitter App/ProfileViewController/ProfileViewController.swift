bbd401570dfc3172de99ef4b7d38ec2e62734abc
eung7 <wannabeeung@gmail.com>
2022-03-22 11:37:29 +0900

    ProfileViewController mock 작업 완료 !

--- eung7 Twitter App/ProfileViewController/ProfileViewController.swift
+++ eung7 Twitter App/ProfileViewController/ProfileViewController.swift
@@ -6,16 +6,52 @@
 //
 
 import UIKit
+import SnapKit
 
 class ProfileViewController : UIViewController {
+    
+    let usernameTextField = UITextField()
+    let accountTextField = UITextField()
+    let saveButton = UIButton()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupNavigationBar()
+        setupLayout()
     }
     
     private func setupNavigationBar() {
         navigationItem.title = "Profile"
     }
+    
+    private func setupLayout() {
+        [ usernameTextField, accountTextField, saveButton ]
+            .forEach { view.addSubview($0) }
+        
+        usernameTextField.text = "KimEungCheol"
+        usernameTextField.font = .systemFont(ofSize: 20, weight: .bold)
+        usernameTextField.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).inset(16)
+            $0.leading.equalToSuperview().inset(16)
+        }
+        
+        accountTextField.text = "eung7"
+        accountTextField.font = .systemFont(ofSize: 16, weight: .regular)
+        accountTextField.snp.makeConstraints {
+            $0.top.equalTo(usernameTextField.snp.bottom).offset(8)
+            $0.leading.equalTo(usernameTextField.snp.leading)
+        }
+        
+        saveButton.setTitle("저장하기", for: .normal)
+        saveButton.titleLabel?.font = .systemFont(ofSize: 14, weight: .bold)
+        saveButton.layer.cornerRadius = 10
+        saveButton.backgroundColor = .systemBlue
+        saveButton.snp.makeConstraints {
+            $0.top.equalTo(accountTextField.snp.bottom).offset(16)
+            $0.leading.equalToSuperview().inset(16)
+            $0.trailing.equalToSuperview().inset(16)
+        }
+    }
 }
+
